b3a8a3e872bd87642539bb7280fd3c8b06392db1
Hiroki <hiroki11hanada@gmail.com>
2022-11-17 01:53:40 +0900

    feature: testing

--- experiment-2.0/experiments/train-ticket/files/pickleloader.py
+++ experiment-2.0/experiments/train-ticket/files/pickleloader.py
@@ -6,13 +6,24 @@ import numpy as np
 
 @click.command('Pickle loader')
 @click.option('-p', '--pickle', 'pickle_path', required=True, multiple=True, type=str)
-def pickle_loader(pickle_path):
-    pickl_list = []
-    for pickle_file in pickle_path:
-        with open(pickle_file, 'rb') as f:
-            pickl_list.extend(pickle.load(f))
+@click.option('-s', '--show', 'show', is_flag=True)
+@click.option('-d', '--is_dataframe', 'is_df', is_flag=True)
+def pickle_loader(pickle_path, show, is_df):
+    if is_df:
+        pickl_list = pd.DataFrame()
+        for pickle_file in pickle_path:
+            with open(pickle_file, 'rb') as f:
+                pickl_list = pd.concat([pickl_list, pickle.load(f)])
+
+    else:
+        pickl_list = []
+        for pickle_file in pickle_path:
+            with open(pickle_file, 'rb') as f:
+                pickl_list.extend(pickle.load(f))
     pp = pprint.PrettyPrinter()
     pp.pprint(len(pickl_list))
+    if show:
+        pp.pprint(pickl_list)
 
 if __name__ == "__main__":
     pickle_loader()
--- experiment-2.0/experiments/train-ticket/files/test.py
+++ experiment-2.0/experiments/train-ticket/files/test.py
@@ -5,22 +5,13 @@ import pprint
 import numpy as np
 
 @click.command('Run TraceRCA')
-@click.option('-h', '--history', 'history_path', required=True, multiple=True, type=str)
-def run_tracerca(history_path):
-    history_df = pd.DataFrame()
-    service_list = []
-    for h in range(len(history_path)):
-        with open(history_path[h], 'rb') as f:
-            h_pkl = pickle.load(f)
-            history_df = pd.concat([history_df, h_pkl])
-            # history_list.extend(h_pkl)
-    history = history_df.set_index(keys=['source', 'target'], drop=True).sort_index()
+@click.option('-p', '--path', 'path', required=True, type=str)
+def run_tracerca(path):
+    selected_features = {}
+    with open(path, 'r') as feat_f:
+        selected_features = eval("".join(feat_f.readlines()))
     pp = pprint.PrettyPrinter()
-    unique_index = np.unique(history.index.values)
-    for source, target in unique_index:
-        service_list.append(source)
-        service_list.append(target)
-    pp.pprint(len(set(service_list)))
+    pp.pprint(selected_features.items())
 
 if __name__ == "__main__":
     run_tracerca()
